d4fc7d5f5bf3260b846bc1f0f9680e74f0418eba
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-10 20:23:10 +0100

    Add: navbar

---
+++ app/components/navbar.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import Logo from "public/logo.png";
+import Image from "next/image";
+// import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">
+        <Image
+          src={Logo}
+          alt="logo"
+          width={45}
+          quality={100}
+          placeholder="blur" // blur version on load
+        />
+      </Link>
+      <Link href="/experience/add">Ajouter</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
