fd15cf40c21221e43e50a4913688d77e6ad70639
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-11 15:30:45 +0100

    Add: add sign up api call

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,14 +3,18 @@ import { apiToken } from "./apiToken";
 
 const axios = require("axios");
 
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
 // Axios config
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Authorization: `bearer ${apiToken}`,
     "Content-Type": "application/json",
   },
 });
+
+// Experiences
 
 export async function getExperiences() {
   try {
@@ -41,3 +45,49 @@ export async function getExperience(id) {
     return null;
   }
 }
+
+// Auth
+
+export async function signUp(_username, _email, _password) {
+  const data = {
+    username: _username,
+    email: _email,
+    password: _password,
+  };
+  console.log(axiosInstance);
+
+  try {
+    const response = await axios.post(baseUrl + "auth/local/register", data);
+
+    if (response.status !== 200) {
+      throw new Error(`Erreur ${response.status}`);
+    }
+    console.log(response.data.data);
+    return response.data.data;
+  } catch (error) {
+    console.log("Error while retrieving data: ", error);
+    return null;
+  }
+}
+
+// export async function signIn(username, email, password) {
+//   try {
+//     const response = await axiosInstance.get("/auth/local/register");
+
+//     if (response.status !== 200) {
+//       throw new Error(`Erreur ${response.status}`);
+//     }
+
+//     return response.data.data;
+//   } catch (error) {
+//     console.log("Error while retrieving data: ", error);
+//     return null;
+//   }
+// }
+
+// export function signOut() {
+//   const router = useRouter();
+//   localStorage.removeItem("jwt");
+//   localStorage.removeItem("username");
+//   router.push("/");
+// }
