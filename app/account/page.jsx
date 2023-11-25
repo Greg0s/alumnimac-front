a518a2b3ec6827dfef10089a891ba416f59015b3
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-25 19:39:28 +0100

    Update: auth dynamic display

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,16 +1,33 @@
 "use client";
 
 import React from "react";
-import SignIn from "../components/signIn";
-import SignUp from "../components/signUp";
-import SignOut from "../components/signOut";
+
+import SignInForm from "../components/signInForm";
+import SignUpForm from "../components/signUpForm";
+import ManageAccount from "../components/manageAccount";
+
+import { useContext } from "react";
+import { AuthContext } from "../utils/authContext";
+import useRenewAccessToken from "../utils/useRenewAccessToken";
 
 export default function Account() {
+  const { authState, setAuthState } = useContext(AuthContext);
+
+  const token = localStorage.getItem("token");
+
+  if (token) useRenewAccessToken(authState, setAuthState);
   return (
     <>
-      <SignUp />
-      {/* <SignIn />
-      <SignOut /> */}
+      {authState ? (
+        <div>
+          <ManageAccount />
+        </div>
+      ) : (
+        <div>
+          <SignUpForm />
+          <SignInForm />
+        </div>
+      )}
     </>
   );
 }
