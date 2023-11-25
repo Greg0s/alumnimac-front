0cdb76464a710a9ce768670757a09f89f1e0d723
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-25 19:32:41 +0100

    Add: AuthContext

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import AuthProvider from "./utils/authContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
---
+++ app/utils/authContext.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext("");
+
+export default function AuthProvider({ children }) {
+  const [authState, setAuthState] = useState();
+
+  return (
+    <AuthContext.Provider value={{ authState, setAuthState }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
