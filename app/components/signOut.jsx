e2300a2447525efbb7e840ac2a4a648413ca057b
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-25 19:36:02 +0100

    Add: sign out component

---
+++ app/components/signOut.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { useContext } from "react";
+import { AuthContext } from "../utils/authContext";
+
+const SignOut = () => {
+  const router = useRouter();
+
+  const { authState, setAuthState } = useContext(AuthContext);
+
+  const signOut = () => {
+    localStorage.removeItem("token");
+    setAuthState(null);
+    router.push("/account");
+  };
+
+  return <button onClick={signOut}>Se déconnecter</button>;
+};
+
+export default SignOut;
