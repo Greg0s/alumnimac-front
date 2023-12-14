bb4a1161cd7f1c6c8b94a0ff619e65a0eaca0dab
Greg0s <gregoire.tinnes@outlook.fr>
2023-12-14 20:27:40 +0100

    Update: experience page new infos

--- app/experience/[id]/page.jsx
+++ app/experience/[id]/page.jsx
@@ -9,10 +9,39 @@ export default async function ExperienceDetails({ params }) {
 
   return (
     <main className="experience-page">
-      <div className="card">
-        <h3>{experience.attributes.position}</h3>
-        <p>{experience.attributes.description}</p>
+      <div className="title">
+        <h1>{experience.attributes.position}</h1>
+        <p className="company">{experience.attributes.company}</p>
       </div>
+      <div className="cards">
+        <div className="block-1">
+          <p>{experience.attributes.type}</p>
+          <p>{experience.attributes.domain}</p>
+        </div>
+        <div className="block-2">
+          <div className="location">
+            <p>
+              {experience.attributes.city}, {experience.attributes.country}
+            </p>
+            {experience.attributes.address && (
+              <p>{experience.attributes.address}</p>
+            )}
+          </div>
+          <div className="name">
+            <p>Prénom Nom</p>
+            <p>IMAC XXX</p>
+          </div>
+          <div className="dates">
+            <p>{experience.attributes.start_date}</p>
+            {experience.attributes.end_date ? (
+              <p>{experience.attributes.end_date}</p>
+            ) : (
+              <p>En cours</p>
+            )}
+          </div>
+        </div>
+      </div>
+      <p className="description">{experience.attributes.description}</p>
     </main>
   );
 }
