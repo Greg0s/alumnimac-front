e48e65d7b42cdf49f80946f4f7608a54ee78db1a
Greg0s <gregoire.tinnes@outlook.fr>
2024-01-23 16:53:35 +0100

    Update: add current user data in context

--- app/utils/authContext.js
+++ app/utils/authContext.js
@@ -1,14 +1,31 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getMe } from "./api";
+import useRenewAccessToken from "./useRenewAccessToken";
 
 export const AuthContext = createContext("");
 
 export default function AuthProvider({ children }) {
   const [authState, setAuthState] = useState();
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const token = localStorage.getItem("token");
+
+  if (token) useRenewAccessToken(authState, setAuthState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (authState) setCurrentUser(await getMe());
+    };
+
+    fetchData();
+  }, [authState]);
 
   return (
-    <AuthContext.Provider value={{ authState, setAuthState }}>
+    <AuthContext.Provider
+      value={{ authState, setAuthState, currentUser, setCurrentUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
