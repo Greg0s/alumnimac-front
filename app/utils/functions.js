80200a08d7c6d07c03cffe93ecee7353c05a2c7c
Greg0s <gregoire.tinnes@outlook.fr>
2024-01-23 16:59:11 +0100

    Add: translation function db to display

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -6,3 +6,48 @@ export function calcDuration(date1, date2) {
 
   return Math.floor(weekDuration);
 }
+
+export function translateAttribute(attributeName, attributeValue) {
+  if (attributeValue == "other") return "Autre";
+
+  switch (attributeName) {
+    case "type":
+      switch (attributeValue) {
+        case "internship":
+          return "Stage";
+        case "job":
+          return "Emploi";
+        default:
+          return "Autre";
+      }
+    case "domain":
+      switch (attributeValue) {
+        case "audiovisual":
+          return "Audiovisuel";
+        case "graphic-design":
+          return "Graphisme";
+        case "project-management":
+          return "Gestion de projet";
+        case "programming":
+          return "Programmation";
+        case "web-dev":
+          return "Dev web/mobile";
+        default:
+          return "Autre";
+      }
+    case "paid":
+      switch (attributeValue) {
+        case true:
+          return "Rémunérée";
+        case false:
+          return "Non-rémunérée";
+      }
+    case "recommended":
+      switch (attributeValue) {
+        case true:
+          return "Recommandée";
+        case false:
+          return "Non-recommandée";
+      }
+  }
+}
