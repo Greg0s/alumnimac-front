ce1d154886cee544cc7800273008ea7b8bbd4e61
Greg0s <gregoire.tinnes@outlook.fr>
2024-01-28 12:29:36 +0100

    Update: experience list content

--- app/views/experiencesList.jsx
+++ app/views/experiencesList.jsx
@@ -1,37 +1,51 @@
 import Link from "next/link";
-// import { getExperiences } from "../utils/api";
-import "../styles/experiencesList.scss";
 import Image from "next/image";
+import "../styles/experiencesList.scss";
+import { translateAttribute } from "../utils/functions";
 
 export default function ExperienceList({ experiences }) {
-  // const filterdByRegion = (array) => {
-  //   return array.filter(el => el.name.common.toLowerCase().includes(query))}
-
   return (
     <div className="experiences-list">
       {experiences &&
         experiences.map((experience) => (
           <div key={experience.id} className="experience">
             <Link href={`/experience/${experience.id}`}>
-              <h3>{experience.attributes.position}</h3>
-              <p>{experience.attributes.company}</p>
+              <div className="title">
+                <h3>{experience.attributes.position}</h3>
+                <span className="title-right">
+                  {"IMAC "}
+                  {experience.attributes.author.data.attributes.graduation_year}
+                  <br />
+                  {translateAttribute("type", experience.attributes.type)}
+                </span>
+              </div>
+              <div className="content">
+                <p className="content-company">
+                  {experience.attributes.company}
+                </p>
+                <p className="content-location">
+                  {experience.attributes.city}, {experience.attributes.country}
+                </p>
+              </div>
             </Link>
           </div>
         ))}
       {experiences && experiences.length === 0 && (
-        <>
+        <div className="no-results">
           <Image
+            className="img"
             src="/cimac.png"
-            alt="Description de l'image"
-            width={200}
-            height={200}
+            alt="C de l'IMAC"
+            width={175}
+            height={175}
           />
-          <p className="text-center">
+          <h2>Pas de résultats :/</h2>
+          <p>
             Il y a une bonne et une mauvaise nouvelle : Tu as trouvé le C de
             l'IMAC, mais il n'y a aucune expérience correspondant à ta
-            recherche...
+            recherche.
           </p>
-        </>
+        </div>
       )}
     </div>
   );
