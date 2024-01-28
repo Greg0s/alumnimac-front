3e4f93cc4330f3a43555b55cd80f1f16d6e01770
Greg0s <gregoire.tinnes@outlook.fr>
2024-01-28 15:43:44 +0100

    Fix: current user condition

--- app/components/accountInfos.jsx
+++ app/components/accountInfos.jsx
@@ -11,29 +11,30 @@ export default function AccountInfos() {
 
   return (
     <>
-      {currentUser && (
-        <div className="account-infos">
-          <h1>Mon compte</h1>
-          <div>
-            <h4>Nom</h4>
-            <p>
-              {currentUser.first_name} {currentUser.last_name}
-            </p>
-          </div>
-          <div>
-            {" "}
-            <h4>Promo</h4>
-            <p>IMAC {currentUser.graduation_year}</p>
-          </div>
-          <div>
-            {" "}
-            <h4>Email</h4>
-            <p>{currentUser.email}</p>
-          </div>
-
-          <SignOut />
-        </div>
-      )}
+      <div className="account-infos">
+        {currentUser && (
+          <>
+            <h1>Mon compte</h1>
+            <div>
+              <h4>Nom</h4>
+              <p>
+                {currentUser.first_name} {currentUser.last_name}
+              </p>
+            </div>
+            <div>
+              {" "}
+              <h4>Promo</h4>
+              <p>IMAC {currentUser.graduation_year}</p>
+            </div>
+            <div>
+              {" "}
+              <h4>Email</h4>
+              <p>{currentUser.email}</p>
+            </div>
+          </>
+        )}
+        <SignOut />{" "}
+      </div>
     </>
   );
 }
